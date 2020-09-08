1ba362f006d124e05eed0abb89077089a1a8dcb7
htz-html <56533301+htz-html@users.noreply.github.com>
2020-09-08 12:40:25 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,16 @@
+console.log(window.Vue)
+console.log(Vue)
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: function (h) { return h(App) },
+// }).$mount('#app')
+new Vue({
+  el:'#app',
+  data:{
+    n:0
+  }
+})
